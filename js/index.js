8b05d48b7f1bfcfb695510b32a27b06d557ccdff
github username <github email address>
2019-10-10 10:12:23 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {});
+function main(){
+    document.addEventListener("DOMContentLoaded", function() {
+        fetchBooks();
+    });
+}
+
+function fetchBooks(){
+    fetch('http://localhost:3000/books')
+    .then(res => res.json())
+    .then(data => {
+        renderBooks(data)
+    })
+}
+
+function renderBooks(books){
+    books.forEach(book => {
+        renderBook(book)
+      })
+}
+
+function renderBook(book){
+    const list = document.getElementById("list");
+    let li = document.createElement("li");
+    li.innerHTML = book.title;
+    li.addEventListener("click", function(event){
+        bookDetails(event.target);
+    });
+    list.append(li);
+}
+
+function bookDetails(book){
+    const show = document.getElementById("show-panel");
+    console.log(book);
+    if(!show.innerHTML && show.innerHTML != `<h2>${book.innerHTML}</h2>`){
+        let h2 = document.createElement('h2');
+        h2.innerHTML = book.innerHTML;
+        show.append(h2);
+    }
+}
+
+main()
