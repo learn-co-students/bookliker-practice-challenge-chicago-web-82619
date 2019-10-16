cfb583ceb394380758be6818eb20f4206ed05e14
github username <github email address>
2019-10-16 11:32:58 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 function main(){
     document.addEventListener("DOMContentLoaded", function() {
+        let books = [];
+        let allUsers = [];
         fetchBooks();
+        fetchUsers();
     });
 }
 
@@ -8,7 +11,17 @@ function fetchBooks(){
     fetch('http://localhost:3000/books')
     .then(res => res.json())
     .then(data => {
+        books = data;
         renderBooks(data)
+    })
+}
+
+function fetchUsers(){
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(data => {
+        allUsers = data;
+        getCurrentUser("pouros");
     })
 }
 
@@ -23,19 +36,107 @@ function renderBook(book){
     let li = document.createElement("li");
     li.innerHTML = book.title;
     li.addEventListener("click", function(event){
-        bookDetails(event.target);
+        currentBook = selectedBook(event.target);
+        bookDetails(currentBook);
     });
     list.append(li);
 }
 
 function bookDetails(book){
     const show = document.getElementById("show-panel");
-    console.log(book);
-    if(!show.innerHTML && show.innerHTML != `<h2>${book.innerHTML}</h2>`){
-        let h2 = document.createElement('h2');
-        h2.innerHTML = book.innerHTML;
-        show.append(h2);
+    show.innerHTML = '';
+    let h2 = document.createElement('h2');
+    h2.innerHTML = book.title;
+    let img = document.createElement("img");
+    img.setAttribute('src',book.img_url);
+    let p = document.createElement('p');
+    p.innerHTML = book.description;
+    let userList = document.createElement('ul');
+    let readBtn = document.createElement('button');
+    readBtn.innerHTML = "Read Book";
+    book.users.forEach (user => {
+        let userLi = document.createElement('li');
+        userLi.innerHTML = user.username;
+        userList.append(userLi);
+        if(user.username == currentUser.username){
+            readBtn.innerHTML = "Remove Book"; 
+        }
+    })
+    readBtn.addEventListener("click", () => {
+        readBook(book);
+    });
+    show.append(h2, img, p, userList, readBtn);
+}
+
+function selectedBook(target){
+    let selectedBook = {};
+    books.forEach(book => {
+        if(book.title == target.innerHTML){
+            selectedBook = book;
+        }
+    })
+    return selectedBook;
+}
+
+function readBook(book){
+    let read = false;
+    book.users.forEach(user => {
+        if(user.username == currentUser.username){
+            read = true;    
+        }
+    })
+    if(read){
+        removeUser(book, currentUser);
+        window.alert("Book removed!");
+    } else {
+        patchBook(book, currentUser);
     }
+}
+
+function patchBook(book, user){
+    let readers = [...book.users, user];
+    fetch(`http://localhost:3000/books/${book.id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify( {
+          users: readers
+        })
+      })
+    .then(resp => resp.json())
+    .then(bookData => {
+        bookDetails(bookData)
+    })
+    
+}
+
+function removeUser(book, user){
+    let readers = book.users; 
+    let filtered = readers.filter(u=>{
+        console.log(u);
+        console.log(user);
+        return u.username != user.username;
+    });
+    fetch(`http://localhost:3000/books/${book.id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify( {
+          users: filtered
+        })
+      })
+    .then(resp => resp.json())
+    .then(bookData => {
+        bookDetails(bookData)
+    })
+    
+}
+
+function getCurrentUser(username){
+    allUsers.forEach(user => {
+        if(user.username == username){
+            currentUser = user;
+        }
+    })
+    return currentUser;
 }
 
 main()
